8f46c9e1c986fcf5b7e7813c106d5ed64bffdeba
Malik Salam <malik.o.salam@gmail.com>
2017-12-04 13:44:42 -0500

    added editors .py files

---
+++ editors/egg.py
@@ -0,0 +1,3 @@
+print("These files are making me hungry")
+print("This file was created using emacs")
+print("I like this interface as well")
---
+++ editors/vinegar.py
@@ -0,0 +1,3 @@
+print("This file was created using vim")
+print("This file was created by Malik")
+print("I am enjoying this modal approach")
